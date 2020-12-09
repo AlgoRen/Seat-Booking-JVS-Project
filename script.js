bb11b5ad7abce200c91251b9cadb70dc09d24844
Algo Ren <algorenstudio@gmail.com>
2020-12-09 00:01:50 -0500

    Used localStorage to save the selectedMovie index, the movie price total, and selectedSeats index in setMovieData function and updateSelectedCount. The populateUI function updates the DOM with the selectedSeats and selectedMovie that we get from localStorage. The most difficult part of this project was trying to figure out what movieSelect.selectedIndex was referring to, it is a built in method that comes with the select element.

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 const container = document.querySelector(".container");
-const seat = document.querySelectorAll(".row .seat:not(.occupied)");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 
 // Update total and count
 function updateSelectedCount() {
   // * Looks for all selected elements in the row with the seat and selected class * //
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
+  // * Copy selected seats into an array
+  // * Map through array
+  // * Return a new array indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
+
+// Get data from localStorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    // The movie is being set to the movie index found in localStorage
+    // * The selectIndex method is a built in method on the select element * //
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // Movie select event
 movieSelect.addEventListener("change", (e) => {
   // * Updating ticket price from the movieSelect value field * //
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
@@ -35,3 +71,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+// Initial count and total set
+updateSelectedCount();
